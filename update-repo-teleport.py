8cfd488f38fc0009d0db25bdd520a18689bf4331
Björn Kötter <github@bk.netburn.de>
2022-09-27 15:06:27 +0200

    Initial commit

--- update-repo-teleport.py
+++ update-repo-teleport.py
@@ -1,11 +1,64 @@
+import os
+import re
+from hashlib import sha256
 from json import loads
-from urllib.request import urlopen
+from pathlib import Path
+from urllib.request import urlopen, urlretrieve
+
+
+def get_config():
+    download_url: str = f'https://get.gravitational.com/teleport-{get_latest_version()}-1.x86_64.rpm'
+    return {
+        'download_url': download_url,
+        'signature_url': download_url + '.sha256',
+        'target_file': '/srv/repo/tools/' + Path(download_url).name,
+    }
+
+
+def get_latest_version() -> str:
+    versions: list = []
+    with urlopen('https://api.github.com/repos/gravitational/teleport/releases') as response:
+        for record in loads(response.read()):
+            if re.match(r'v[.\d]+$', record['tag_name']):
+                versions.append(tuple([int(n) for n in re.sub(r'^v', '', record['tag_name']).split('.')]))
+    return '.'.join(str(n) for n in sorted(versions)[-1])
+
+
+def teleport_file_download(config: dict):
+    if Path(config['target_file']).exists():
+        print(f"Latest version already exists: '{config['target_file']}'. Not downloading.")
+    else:
+        print(f'Downloading from {config["download_url"]} to {config["target_file"]}')
+        urlretrieve(config["download_url"], config["target_file"])
+
+
+def get_sha256sum_file(file: str) -> str:
+    sha256sum = sha256()
+    with open(file, 'rb') as fh:
+        while True:
+            data = fh.read(65536)
+            if not data:
+                return sha256sum.hexdigest()
+            sha256sum.update(data)
+
+
+def teleport_signature_verify(config: dict):
+    print(f'Comparing signature of {config["target_file"]} with {config["signature_url"]}')
+    with urlopen(config['signature_url']) as response:
+        sha256sum_teleport = response.read(64).decode("utf-8")
+
+    sha256sum_dl_file = get_sha256sum_file(config["target_file"])
+    if sha256sum_dl_file == sha256sum_teleport:
+        print(f'File {config["target_file"]} has a valid checksum')
+    else:
+        print(f'File {config["target_file"]} has an invalid checksum. Deleting file.')
+        os.remove(config['target_file'])
 
 
 def main():
-    with urlopen('https://api.github.com/repos/gravitational/teleport/releases') as response:
-        for record in loads(response.read()):
-            print(record['tag_name'])
+    config: dict = get_config()
+    teleport_file_download(config)
+    teleport_signature_verify(config)
 
 
 if __name__ == '__main__':
